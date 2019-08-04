7ce0b8f9ff460fdbaa97716251c63e205c31e31c
Ricky Lau <rickylaufitness@gmail.com>
2019-08-04 11:04:43 -0400

    added anagram algo

---
+++ anagrams.js
@@ -0,0 +1,38 @@
+// Write a function that takes in two strings and returns a boolean if both strings are anagrams of each other.
+// Ex) ('','') => true
+//     ('car', 'ra') => false
+//     ('anagram, 'naagram') => true
+
+// TIME COMPLEXITY
+// O(n) time complexity
+
+// SPACE COMPLEXITY
+// O(1) space complexity
+
+const anagram = (str1, str2) => {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let countOne = {};
+  let countTwo = {};
+
+  for (let val of str1) {
+    if (!countOne[val]) countOne[val] = 1;
+    else countOne[val]++;
+  }
+
+  for (let val of str2) {
+    if (!countTwo[val]) countTwo[val] = 1;
+    else countTwo[val]++;
+  }
+
+  for (let key in countOne) {
+    if (!(key in countTwo)) return false;
+    if (countTwo[key] !== countOne[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
