82b44e9c6a8a1ecf96e0ed010fccf33119ebafa4
Ricky Lau <rickylaufitness@gmail.com>
2019-08-08 14:17:19 -0400

    added capitalize words in array algo

---
+++ recursion/capitalizeWords.js
@@ -0,0 +1,13 @@
+// write a function capitalize words which takes in an array of strings and returns a new array of capitalized string values of first array.
+
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
+}
