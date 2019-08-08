f068b91f2950d5d8f398319d343e599534f9cbdf
Ricky Lau <rickylaufitness@gmail.com>
2019-08-08 12:05:05 -0400

    added refactor version

--- recursion/someRecursive.js
+++ recursion/someRecursive.js
@@ -20,3 +20,9 @@ function someRecursive(arr, callback) {
 
   return false;
 }
+
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
