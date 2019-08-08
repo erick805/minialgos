da5c915beb4b7c03eca45851489fcad3281282ef
Ricky Lau <rickylaufitness@gmail.com>
2019-08-08 14:08:03 -0400

    added sum of even values in nested object algo

---
+++ recursion/nestedEvenSum.js
@@ -0,0 +1,35 @@
+// write a function nested even sum which takes in a nested object and returns the sum of all even integer values inside the nested object.
+
+// let obj1 = {
+//   outer: 2,
+//   obj: {
+//     inner: 2,
+//     otherObj: {
+//       superInner: 2,
+//       notANumber: true,
+//       alsoNotANumber: "yup"
+//     }
+//   }
+// }
+
+// let obj2 = {
+//   a: 2,
+//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//   d: 1,
+//   e: {e: {e: 2}, ee: 'car'}
+// };
+
+// nestedEvenSum(obj1); // 6
+// nestedEvenSum(obj2); // 10
+
+function nestedEvenSum(obj, sum = 0) {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      sum += nestedEvenSum(obj[key]);
+    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
