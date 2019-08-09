a144bcc3ee25b4ae58829b882be164f4b9a64d50
Ricky Lau <rickylaufitness@gmail.com>
2019-08-09 16:42:20 -0400

    added some commented out code

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -11,12 +11,14 @@
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = i - 1; j >= 0; j--) {
+      // console.log(arr, arr[j], arr[i])
       if (arr[i] <= arr[j]) {
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
       }
     }
+    // console.log('PASS COMPLETE!)
   }
   return arr;
 }
