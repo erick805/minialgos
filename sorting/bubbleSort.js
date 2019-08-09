7a942549895febb4bfa3c52006bc5d83ebfb01e1
Ricky Lau <rickylaufitness@gmail.com>
2019-08-09 16:58:24 -0400

    optimized with no swaps boolean & added time comexplity

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -8,6 +8,9 @@
 // if arr[j] is greater than arr[j + 1], swap the values.
 // return the sorted array.
 
+// TIME COMPLEXITY
+// O(n)^2
+
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = i - 1; j >= 0; j--) {
@@ -23,17 +26,22 @@ function bubbleSort(arr) {
   return arr;
 }
 
+// Optimized with no swaps
+
 const bubbleSort = arr => {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
-
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 };
