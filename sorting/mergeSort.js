c3b90c5b37ddf8b2cf2ea72caec457e3911139ce
Ricky Lau <rickylaufitness@gmail.com>
2019-08-11 13:01:43 -0400

    added merge function

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -15,3 +15,27 @@
 // if the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to next.
 // if the value in first is larger than the value in second, push the value from second into results and move on to next value in second array.
 // once we exhaust one array, push all remaining values from other array.
+
+function merge(arr1, arr2) {
+  let mergedArray = [];
+  let pt1 = 0;
+  let pt2 = 0;
+
+  while (pt1 < arr1.length && pt2 < arr2.length) {
+    if (arr1[pt1] <= arr2[pt2]) {
+      mergedArray.push(arr1[pt1]);
+      pt1++;
+    } else {
+      mergedArray.push(arr2[pt2]);
+      pt2++;
+    }
+  }
+
+  if (arr1.length <= arr2.length) {
+    mergedArray.concat(arr2);
+  } else {
+    mergedArray.concat(arr1);
+  }
+
+  return mergedArray;
+}
