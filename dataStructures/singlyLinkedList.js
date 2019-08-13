36283203cc66745158ef24ce30ab78e704333a75
Ricky Lau <rickylaufitness@gmail.com>
2019-08-13 11:42:53 -0400

    added singly linked list class and push instance method

---
+++ dataStructures/singlyLinkedList.js
@@ -0,0 +1,33 @@
+// Singly Linked List
+
+// A data structure that contains a head, tail, and length property.
+
+// Linked List consists of nodes, and each node has a value and a pointer to another node or null.
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+}
