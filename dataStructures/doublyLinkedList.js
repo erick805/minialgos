9cc0c47b00f8ca496d5e7c0747c5a671595ca861
Ricky Lau <rickylaufitness@gmail.com>
2019-08-14 15:54:19 -0400

    added set instance method for doubly linked list

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -92,4 +92,13 @@ class DoublyLinkedList {
     }
     return current;
   }
+
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
