b1055d80814ba5e167114cbdb6905a09c55c5e11
Ricky Lau <rickylaufitness@gmail.com>
2019-08-14 11:07:09 -0400

    added pop instance method for doubly linked list

---
+++ dataStructures/doublyLinkedList.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+    return currentTail;
+  }
+}
