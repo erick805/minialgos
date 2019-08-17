338264c3d6da545203d727a9711c90d09172e4f4
Ricky Lau <rickylaufitness@gmail.com>
2019-08-16 21:40:53 -0400

    added find instance method for bst class

--- dataStructures/BST.js
+++ dataStructures/BST.js
@@ -34,4 +34,20 @@ class BST {
       }
     }
   }
+
+  find(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        return !found;
+      }
+    }
+    return found;
+  }
 }
