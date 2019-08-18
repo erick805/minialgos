7b128a86cf15e49e8b0408088508d1fa2ddd839b
Ricky Lau <rickylaufitness@gmail.com>
2019-08-18 13:07:53 -0400

    added priority que and node class for priority que algo

---
+++ dataStructures/priorityQue.js
@@ -0,0 +1,14 @@
+// Min Binary Heap
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class priorityQue {
+  constructor() {
+    this.values = [];
+  }
+}
