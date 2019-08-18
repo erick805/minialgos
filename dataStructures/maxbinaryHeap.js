06bc289e10ee7ace9b12b0af9c56a754e5125323
Ricky Lau <rickylaufitness@gmail.com>
2019-08-17 22:46:39 -0400

    added insert max binary heap instance method

--- dataStructures/maxbinaryHeap.js
+++ dataStructures/maxbinaryHeap.js
@@ -11,8 +11,28 @@
 
 // A binary heap is as compact as possible. All the children of each node are as full as they can be and left children are filled out first.
 
-class maxBinaryHeap {
+class MaxBinaryHeap {
   constructor() {
-    this.values = [];
+    this.values = [41, 39, 33, 18, 27, 12, 55];
+  }
+
+  insert(val) {
+    this.values.push(val);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+
+    let childVal = this.values[index];
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parentVal = this.values[parentIndex];
+      if (childVal <= parentVal) break;
+      this.values[parentIndex] = childVal;
+      this.values[index] = parentVal;
+      index = parentIndex;
+    }
   }
 }
