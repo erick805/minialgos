7073bab4e19d7c93849e58fb16c125a24d649ef7
Ricky Lau <rickylaufitness@gmail.com>
2019-08-19 11:04:08 -0400

    added graph data structure and comments

---
+++ dataStructures/graph.js
@@ -0,0 +1,25 @@
+// Graphs
+
+// A graph structure consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph.
+
+// Nodes + Connections
+
+// Uses for graphs
+// social networks
+// location/mapping
+// routing algorithims
+// visual hierarchy
+// file system optimizations
+// EVERYWHERE!
+
+// Undirected Graph
+// no direction to the connections(edge)
+
+// Directed Graph
+// direction is involved in connections(edge)
+
+// Weighted Graph
+// each edge has a weight associated with it.
+
+// Unweighted Graph
+// each edge has no weight associated with it.
