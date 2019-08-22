8c9075a5f1357b9fe9e21c6741b0531891001313
Ricky Lau <rickylaufitness@gmail.com>
2019-08-22 10:36:26 -0400

    added priorityqueue class to weighted graph class

--- dataStructures/weightedGraph.js
+++ dataStructures/weightedGraph.js
@@ -16,3 +16,22 @@ class WeightedGraph {
     });
   }
 }
+
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(val, priority) {
+    this.queue.push({ val, priority });
+    this.sort();
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  sort() {
+    this.queue.sort((a, b) => a.priority - b.priority);
+  }
+}
