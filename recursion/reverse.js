295ed2fd16bcb5e3d48ef9d77f1ff53a3636f33a
Ricky Lau <rickylaufitness@gmail.com>
2019-08-30 19:36:45 -0400

    added alternate iterative solution

--- recursion/reverse.js
+++ recursion/reverse.js
@@ -3,7 +3,31 @@
 // reverse('awesome') // 'emosewa'
 // reverse('rithmschool') // 'loohcsmhtir'
 
+// TIME COMPLEXITY
+// O(n)
+// SPACE COMPLEXITY
+// O(n)
+
 function reverse(str) {
   if (str.length <= 1) return str;
   return reverse(str.slice(1)) + str[0];
+}
+
+// TIME COMPLEXITY
+// O(n)
+// SPACE COMPLEXITY
+// O(n + m)
+function reverse(str) {
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "cant be reversed";
+  }
+  str = str.split("");
+
+  let returnStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    returnStr += str[i];
+  }
+
+  return returnStr;
 }
