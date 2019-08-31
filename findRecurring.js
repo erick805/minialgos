dc9cf9d60c2f3c8c442efebc7c0c2f9bd9f8a2cc
Ricky Lau <rickylaufitness@gmail.com>
2019-08-31 13:23:26 -0400

    added find first recurring number in array algo

---
+++ findRecurring.js
@@ -0,0 +1,26 @@
+// Write a function that takes in an array and returns the first recurring number in the array.
+
+// Ex findRecurring([2,5,1,2,3,5,1,2,4]) => 2
+//                 ([2,1,1,2,3,5,1,2,4]) => 1
+//                 ([2,3,4,5]) => undefined
+
+// TIME COMPLEXITY
+// O(n)
+
+// SPACE COMPLEXITY
+// O(n)
+
+function findRecurring(arr) {
+  // loop through the array and save each value as a key with a boolean true if seen.
+  // if the dictionary contains a key with a boolean true on the next iteration of array.
+  // we can return the key in dictionary or value of the array.
+  // if no value is seen, return undefined.
+  const seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (!seen[num]) seen[num] = true;
+    else if (seen[num]) return num;
+  }
+
+  return undefined;
+}
