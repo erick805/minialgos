a164c0bf3956afcf0403d92aa78efbdc0722f454
Ricky Lau <rickylaufitness@gmail.com>
2019-09-14 14:54:10 -0400

    added fib iterative approach

--- recursion/fib.js
+++ recursion/fib.js
@@ -4,6 +4,16 @@
 // (10) => 55
 // (28) => 317811
 
+// fib iterative
+function fib(num) {
+  let fibArr = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+  }
+  return fibArr[num];
+}
+// fib recursive
 function fib(num) {
   if (num <= 2) return 1;
   return fib(num - 1) + fib(num - 2);
