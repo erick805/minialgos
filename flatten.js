a0513bec20c1df25e15f782d147e4d94be8423e2
Ricky Lau <rickylaufitness@gmail.com>
2019-10-07 17:47:03 -0400

    added flatten algo solution

--- flatten.js
+++ flatten.js
@@ -1,5 +1,6 @@
 /*
 Given a binary tree, flatten it to a linked list in-place.
+Do not return anything, modify root in-place instead.
 
 For example, given the following tree:
 
@@ -23,3 +24,33 @@ The flattened tree should look like:
          \
           6
 */
+
+/*
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+function flatten(root) {
+  let head = root;
+  let stack = [];
+
+  while (head) {
+    if (head.right) {
+      stack.push(head.right);
+      head.right = null;
+    }
+    if (head.left) {
+      head.right = head.left;
+      head.left = null;
+    }
+
+    if (!head.left && !head.right && stack.length > 0) {
+      let node = stack.pop();
+      head.right = node;
+    }
+    head = head.right;
+  }
+}
