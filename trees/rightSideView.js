e9b3817270a0000f38dc1a40135bbb6f4de0a9bf
Ricky Lau <rickylaufitness@gmail.com>
2019-10-09 12:47:49 -0400

    added right side view bfs solution

--- trees/rightSideView.js
+++ trees/rightSideView.js
@@ -13,3 +13,28 @@ Explanation:
  \     \
   5     4       <---
 */
+
+// top to bottom usually means BFS
+// right most node on each level is what we will add to our visible list.
+
+function rightSideView(root) {
+  if (!root) return [];
+  let next = [{ node: root, level: 0 }];
+  let visibleValues = [];
+
+  while (next.length) {
+    let curr = next.pop();
+    if (!curr.node) continue;
+    if (curr.level === visibleValues.length) {
+      visibleValues.push(curr.node.val);
+    }
+    if (curr.node.left) {
+      next.push({ node: curr.node.left, level: curr.level + 1 });
+    }
+    if (curr.node.right) {
+      next.push({ node: curr.node.right, level: curr.level + 1 });
+    }
+  }
+
+  return visibleValues;
+}
