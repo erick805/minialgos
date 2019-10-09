ea21f660875cdc223815462e1b007bc823bf093e
Ricky Lau <rickylaufitness@gmail.com>
2019-10-09 13:05:21 -0400

    added pseudocode for right side view in tree algo

--- trees/rightSideView.js
+++ trees/rightSideView.js
@@ -24,23 +24,30 @@ Explanation:
 //O(n) space
 
 function rightSideView(root) {
-  if (!root) return [];
+  // set a queue for bfs, initiate node as root, and current level as zero
   let next = [{ node: root, level: 0 }];
+  // set a visibleValues array to return
   let visibleValues = [];
 
+  // while our priority queue has a length
   while (next.length) {
+    // lets pop off our most recent node in our queue
     let curr = next.pop();
+    // if there is no node, move to the next iteration.
     if (!curr.node) continue;
+    // if the curr node's level is equal to our length for visibleValues, we know that we are on the same level!, we can only have a maximum of one node for each level in our visible values array!
     if (curr.level === visibleValues.length) {
       visibleValues.push(curr.node.val);
     }
+    // bfs check if curr node has a left, and add it to the queue and increment curr node's left level by 1
     if (curr.node.left) {
       next.push({ node: curr.node.left, level: curr.level + 1 });
     }
+    // likewise if our curr node has a right, add it to the queue and increment our curr node's right level by 1 ** our right node will be the most recent in our queue ** // this ensures we take the right most element and add it to our visibles array.
     if (curr.node.right) {
       next.push({ node: curr.node.right, level: curr.level + 1 });
     }
   }
-
+  // return our right side view
   return visibleValues;
 }
