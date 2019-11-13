27a00f8fe93d6aab69e7eac035d5fed9d4018a83
Ricky Lau <rickylaufitness@gmail.com>
2019-11-13 16:45:02 -0500

    added find quadruplet in array equal to sum algo in multiple pointers dir

---
+++ multiplePointers/findArrayQuadruplet.js
@@ -0,0 +1,20 @@
+/*
+Given an unsorted array of integers arr and a target, write a function findArrayQuadruplet that finds four numbers (quadruplet) in arr
+thats sums up to target. Your function should return an array of these numbers in an ascending order. If no quadruplet exists, return an empty
+array.
+
+Note: There might be more than one quadruplet in arr whose sum is target. You have to return the first one you encounter (considering results sorted)
+
+Input: unsorted arr, target number
+Output: [4 sorted ascended values === target] *output first*
+
+input:  arr = [2, 7, 4, 0, 9, 5, 1, 3], target = 20
+
+output: [0, 4, 7, 9]
+
+input: arr = [1] target = 1
+output: []
+
+input: arr =[-1,-5,0,0,5] target = 0
+output: [-5,0,0,5]
+*/
