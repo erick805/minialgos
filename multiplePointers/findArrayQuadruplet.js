0e9b1a33a42ada116be078d906b61c15646ff6b7
Ricky Lau <rickylaufitness@gmail.com>
2019-11-13 17:18:36 -0500

    added pseudocode for multiple pointers approach in find array quadruplet

--- multiplePointers/findArrayQuadruplet.js
+++ multiplePointers/findArrayQuadruplet.js
@@ -20,23 +20,30 @@ output: [-5,0,0,5]
 */
 
 function findArrayQuadruplet(arr, target) {
+  // if array length is less than four return an empty array
   if (arr.length < 4) return [];
-
+  // sort array in ascending order
   arr.sort((a, b) => a - b);
-
+  // loop through the array and set first pointer
   for (let i = 0; i <= arr.length - 4; i++) {
+    // loop through and set second pointer
     for (let j = i + 1; j <= arr.length - 3; j++) {
+      // find remainder of target === sum of first + second pointer
       let remainder = target - (arr[i] + arr[j]);
-
+      // initiate our third and fourth pointer
       let low = j + 1;
       let high = arr.length - 1;
-
+      // if our pointers don't collide we have possiblities
       while (high > low) {
+        // if our third + fourth pointer (second half) < remainder, increase third pointer to right
         if (arr[low] + arr[high] < remainder) low++;
+        // if our third + fourth pointer (second half) > remainder, decrease fourth pointer to left
         else if (arr[low] + arr[high] > remainder) high--;
+        // else our remainders are equal - just return the array values from first to fourth, since it is sorted.
         else return [arr[i], arr[j], arr[low], arr[high]];
       }
     }
   }
+  // return an empty array if we don't have valid quadruplets
   return [];
 }
