35c81bfcc1839cef0184f57b599fa38a2a720ec5
Ricky Lau <rickylaufitness@gmail.com>
2019-11-14 16:38:45 -0500

    edited: time and space complexity layout and added multiple pointers approach to get indices of weight

--- getIndicesOfWeights.js
+++ getIndicesOfWeights.js
@@ -1,5 +1,5 @@
 /*
-Given a package with a weight limit limit and an array arr of item weights, implement a function getIndicesOfItemWeights that finds two items whose sum of weights equals the weight limit limit. Your function should return a pair [i, j] of the indices of the item weights, ordered such that i > j. If such a pair doesn’t exist, return an empty array.
+Given a package with a weight limit and an array arr of item weights, implement a function getIndicesOfItemWeights that finds two items whose sum of weights equals the weight limit. Your function should return a pair [i, j] of the indices of the item weights, ordered such that i > j. If such a pair doesn’t exist, return an empty array.
 */
 
 // input = 2 arguments, first arg = arr, second arg = limit integer
@@ -13,11 +13,13 @@ Given a package with a weight limit limit and an array arr of item weights, impl
 // ([3,1], 5) => []
 // ([3,2], 5) => [1,0]
 
+// Brute Force solution
+
 // Time Complexity
-// O(n^2) time
+// O(n^2)
 
 // Space Complexity
-// O(1) space
+// O(1)
 
 function getIndicesOfItemWeights(arr, limit) {
   for (let i = arr.length - 1; i >= 0; i--) {
@@ -29,15 +31,16 @@ function getIndicesOfItemWeights(arr, limit) {
   }
   return [];
 }
-// loop through the array
+
+// Hash Table solution
 // save it in a hash map with the current value as the key and the index as the value.
 // check if the difference is already a key, if it is just return that index, and your current index
 
-//Time Complexity
-//O(n) time
+// Time Complexity
+// O(n)
 
-//Space Complexity
-//O(n) space
+// Space Complexity
+// O(n)
 
 function getIndicesOfItemWeights(arr, limit) {
   const hash = {};
@@ -51,5 +54,29 @@ function getIndicesOfItemWeights(arr, limit) {
       hash[currVal] = i;
     }
   }
+  return [];
+}
+
+// Multiple pointers solution
+
+// Time Complexity
+// O(logn)
+
+// Space Complexity
+// O(1)
+
+function getIndicesOfItemWeights(arr, limit) {
+  if (arr.length < 2) return [];
+
+  let first = 0;
+  let second = arr.length - 1;
+
+  while (second > first) {
+    let sumPackages = arr[first] + arr[second];
+    if (sumPackages === limit) return [second, first];
+    else if (sumPackages > limit) second--;
+    else first++;
+  }
+
   return [];
 }
