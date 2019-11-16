22d52a0ba7e3dc31f70363390bd683d270f9f8ee
Ricky Lau <rickylaufitness@gmail.com>
2019-11-16 13:10:17 -0500

    add: word count engine prompt into frequency counter dir

---
+++ frequencyCounter/wordCountEngine.js
@@ -0,0 +1,6 @@
+/*
+Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all unqiue words in it and their number of occurences, sorted by the number of occurences in a descending order. If two or more words have the same count, they should be sorted according to their order in the original sentence. Assume that all letters are in english alphabet. The function should be case-insentitive, so for instance, the words "Perfect" and "perfect" should be considered same word.
+
+The engine should strip out punctuation (even in the middle of a word) and use whitespaces to seperate words.
+
+*/
