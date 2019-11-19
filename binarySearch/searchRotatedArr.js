40fd6908b40fd4c23d98a1abedddfb0ddcef8d8a
Ricky Lau <rickylaufitness@gmail.com>
2019-11-19 10:50:55 -0500

    add: search rotated sorted array prompt and example into binary search dir

---
+++ binarySearch/searchRotatedArr.js
@@ -0,0 +1,15 @@
+/*
+Suppose you have a sorted array in ascending order that is rotated at some pivot unknown to you beforehand. (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+You are given a target value to search. If found in the array return it's index, otherwise return -1.
+You may assume no duplicates exist in the array.
+The algorithm's runtime complexity must be O(logn) or less.
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+*/
