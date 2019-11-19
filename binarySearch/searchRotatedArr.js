ebf02141236ac67fe42ec2cd9c826ad394449a62
Ricky Lau <rickylaufitness@gmail.com>
2019-11-19 14:03:32 -0500

    add: binary search modular approach to search rotated array algo

--- binarySearch/searchRotatedArr.js
+++ binarySearch/searchRotatedArr.js
@@ -13,3 +13,33 @@ Example 2:
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 */
+
+const searchRotatedArr = (rotatedArr, target) => {
+  const peak = findPeak(rotatedArr);
+
+  return rotatedArr[0] > target
+    ? binarySearch(rotatedArr, target, peak + 1)
+    : binarySearch(rotatedArr, target, 0, peak + 1);
+};
+
+const findPeak = (arr, left = 0, right = arr.length) => {
+  let peak = Math.floor((right + left) / 2);
+
+  while (arr[peak - 1] > arr[peak] || arr[peak + 1] > arr[peak]) {
+    arr[0] > arr[peak] ? (right = peak) : (left = peak);
+    peak = Math.floor((right + left) / 2);
+  }
+
+  return peak;
+};
+
+const binarySearch = (arr, target, left = 0, right = arr.length) => {
+  let mid = Math.floor((right + left) / 2);
+
+  while (arr[mid] !== target && Math.abs(right - left) > 1) {
+    arr[mid] > target ? (right = mid) : (left = mid);
+    mid = Math.floor((right + left) / 2);
+  }
+
+  return arr[mid] === target ? mid : -1;
+};
