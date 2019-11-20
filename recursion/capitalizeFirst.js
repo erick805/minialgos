a906ccecf6179789d559b520fb46e8f4db8604b0
Ricky Lau <rickylaufitness@gmail.com>
2019-11-19 22:07:33 -0500

    edit: refactor to es6 arrow function and spread, let to const

--- recursion/capitalizeFirst.js
+++ recursion/capitalizeFirst.js
@@ -1,22 +1,22 @@
-// write a function capitalize first which takes in an array of strings and capitalizes the first letter of each string value.
+// Write a function capitalize first which takes in an array of strings and capitalizes the first letter of each string value.
 
 // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
 
-function capitalizeFirst(arr) {
-  let capitalArray = [];
+const capitalizeFirst = arr => {
+  const capitalArray = [];
   if (arr.length === 0) return [];
 
   while (arr.length) {
     if (arr[0][0]) {
       let capitalLetter = arr[0][0].toUpperCase();
 
-      let capitalWord = (capitalLetter += arr[0].slice(1));
-      capitalArray = capitalArray.concat(capitalWord);
+      const capitalWord = (capitalLetter += arr[0].slice(1));
+      capitalArray.push(capitalWord);
 
       arr = arr.slice(1);
-      return capitalArray.concat(capitalizeFirst(arr));
+      return [...capitalArray, ...capitalizeFirst(arr)]; //capitalArray.concat(capitalizeFirst(arr));
     }
   }
 
   return capitalArray;
-}
+};
