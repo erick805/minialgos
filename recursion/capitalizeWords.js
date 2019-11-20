009ed4d2911ed3283c2706b71cfa5f19765401d2
Ricky Lau <rickylaufitness@gmail.com>
2019-11-19 22:13:16 -0500

    edit: grammar fix in prompt, refactor to es6 function, change let to const

--- recursion/capitalizeWords.js
+++ recursion/capitalizeWords.js
@@ -1,13 +1,13 @@
-// write a function capitalize words which takes in an array of strings and returns a new array of capitalized string values of first array.
+// Write a function capitalize words which takes in an array of strings and returns a new array of capitalized string values of first array.
 
-// let words = ['i', 'am', 'learning', 'recursion'];
+// const words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
 
-function capitalizeWords(array) {
+const capitalizeWords = array => {
   if (array.length === 1) {
     return [array[0].toUpperCase()];
   }
-  let res = capitalizeWords(array.slice(0, -1));
+  const res = capitalizeWords(array.slice(0, -1));
   res.push(array.slice(array.length - 1)[0].toUpperCase());
   return res;
-}
+};
