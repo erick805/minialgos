b08b131ac555ca401b7cd6029ad5d8fd04e01826
Ricky Lau <rickylaufitness@gmail.com>
2019-11-23 17:47:03 -0500

    add: multiple pointers solution to remove nth from end

--- multiplePointers/removeNthFromEnd.js
+++ multiplePointers/removeNthFromEnd.js
@@ -10,3 +10,20 @@ Note:
 
 Given n will always be valid.
 */
+
+const removeNthFromEnd = (head, n) => {
+  const nullHead = new Listnode(null);
+  nullHead.next = head;
+
+  let follow = nullHead;
+  let guide = nullHead;
+
+  for (let i = 1; i <= n + 1; i++) guide = guide.next;
+
+  while (guide !== null) {
+    guide = guide.next;
+    follow = follow.next;
+  }
+  follow.next = follow.next.next;
+  return nullHead.next;
+};
