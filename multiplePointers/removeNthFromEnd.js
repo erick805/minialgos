0203569e4109e59e60178330cf052cbcb3f93281
Ricky Lau <rickylaufitness@gmail.com>
2019-11-23 17:53:18 -0500

    add: pseudocode for multiple pointers approach in remove nth from end in single ll

--- multiplePointers/removeNthFromEnd.js
+++ multiplePointers/removeNthFromEnd.js
@@ -12,18 +12,21 @@ Given n will always be valid.
 */
 
 const removeNthFromEnd = (head, n) => {
+  // create dummy head
   const nullHead = new Listnode(null);
   nullHead.next = head;
-
+  // initiate two pointers one to guide and one to follow
   let follow = nullHead;
   let guide = nullHead;
-
+  // loop until we go one more than the n, this is to keep an indicator of prev node of nth node from end
   for (let i = 1; i <= n + 1; i++) guide = guide.next;
-
+  // while guide is not at the end, reassign follow to be equal to prev node of nth node from end
   while (guide !== null) {
     guide = guide.next;
     follow = follow.next;
   }
+  // set the prev node next to nth node's next.
   follow.next = follow.next.next;
+  // return dummy head
   return nullHead.next;
 };
