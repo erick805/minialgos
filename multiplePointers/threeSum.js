79f8b826f4cd50298b898f836de2885ad5a5f2d8
Ricky Lau <rickylaufitness@gmail.com>
2019-11-30 15:41:17 -0500

    add: pseudocode for three sum in array multiple pointers approach

--- multiplePointers/threeSum.js
+++ multiplePointers/threeSum.js
@@ -17,21 +17,30 @@ A solution set is:
 */
 
 const threeSum = nums => {
+  // sort from least to greatest
   nums.sort((a, b) => a - b);
 
   const solutions = [];
 
   for (let i = 0; i < nums.length - 2; i++) {
+    // if our first pointer is positive we can break since it is sorted
     if (nums[i] > 0) break;
+    // if we are past 1 we do not want to repeat duplicates for i
     if (i > 0 && nums[i] === nums[i - 1]) continue;
+    // initiate two pointers - left and right
     let left = i + 1;
     let right = nums.length - 1;
+    // loop through other half of array
     while (left < right) {
+      // capture current sum
       const sum = nums[i] + nums[left] + nums[right];
+      // move left and right based on comparison to zero
       if (sum > 0) right--;
       else if (sum < 0) left++;
       else {
+        // else we found it, push into solutions array
         solutions.push([nums[i], nums[left], nums[right]]);
+        // check to make sure we are not repeating duplicates for left and right
         while (left < right && nums[left] === nums[left + 1]) left++;
         while (left < right && nums[right] === nums[right - 1]) right--;
         left++;
