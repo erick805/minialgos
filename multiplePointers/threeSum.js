f4c5fbd50eda3b2c32adb1f60c85c8f6050f0c13
Ricky Lau <rickylaufitness@gmail.com>
2019-11-30 15:46:26 -0500

    add: time and space complexity for three sum multiple pointers solution

--- multiplePointers/threeSum.js
+++ multiplePointers/threeSum.js
@@ -16,6 +16,9 @@ A solution set is:
 ]
 */
 
+// Time Complexity: O(n^2) worst case
+// Space Complexity: O(1)
+
 const threeSum = nums => {
   // sort from least to greatest
   nums.sort((a, b) => a - b);
