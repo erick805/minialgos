a8f0044514fafb057c7787a426289901d4dfc917
Ricky Lau <rickylaufitness@gmail.com>
2019-11-30 15:37:06 -0500

    add: three sum multiple pointers solution

--- multiplePointers/threeSum.js
+++ multiplePointers/threeSum.js
@@ -15,3 +15,30 @@ A solution set is:
   [-1, -1, 2]
 ]
 */
+
+const threeSum = nums => {
+  nums.sort((a, b) => a - b);
+
+  const solutions = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum > 0) right--;
+      else if (sum < 0) left++;
+      else {
+        solutions.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      }
+    }
+  }
+
+  return solutions;
+};
