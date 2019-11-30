0216d162e38301cb11f4f2da69f1ba6fd7cf2fa6
Ricky Lau <rickylaufitness@gmail.com>
2019-11-30 15:25:01 -0500

    add: three sum in array algo prompt and example into multiple pointers dir

---
+++ multiplePointers/threeSum.js
@@ -0,0 +1,17 @@
+/*
+Given an array of n integers, return a set of elemenets a, b, c such that a + b + c = 0.
+Find all unique triplets in the array which gives the sum of zero. (permutations not neccessary)
+
+Note:
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4]
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
