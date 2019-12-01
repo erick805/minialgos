a7037287fb32738a3875599e7c1120182e7d5c6c
Ricky Lau <rickylaufitness@gmail.com>
2019-11-30 22:10:22 -0500

    add: multiple pointers solution & pseudocode to next permutation algo

--- multiplePointers/nextPermutation.js
+++ multiplePointers/nextPermutation.js
@@ -11,3 +11,40 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 */
+
+const nextPermutation = nums => {
+  if (!nums || nums.length === 0) return nums;
+
+  let option = -1;
+  // find start of first decreasing sub sequence starting from back
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) {
+      option = i;
+      break;
+    }
+  }
+  // if their is an option to permutate loop from end to option
+  if (option >= 0) {
+    for (let i = nums.length - 1; i > option; i--) {
+      // as soon as we find a number that is greater than option swap since it is least on right side
+      if (nums[i] > nums[option]) {
+        let temp = nums[i];
+        nums[i] = nums[option];
+        nums[option] = temp;
+        break;
+      }
+    }
+  }
+  // reverse back again to get the lowest possible permutation for option.
+  reverse(option + 1, nums.length - 1, nums);
+};
+
+const reverse = (beg, end, arr) => {
+  while (beg < end) {
+    let temp = arr[beg];
+    arr[beg] = arr[end];
+    arr[end] = temp;
+    beg++;
+    end--;
+  }
+};
