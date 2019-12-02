7aa09f5dd1254d5b09fbc5c7606e22cb8a5c6005
Ricky Lau <rickylaufitness@gmail.com>
2019-12-02 15:45:45 -0500

    add: multiply string solution and pseudocode

--- multiplyStr.js
+++ multiplyStr.js
@@ -18,3 +18,42 @@ Note:
 3. Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 4. You must not use any built-in BigInteger library or convert the inputs to integer directly.
 */
+
+const multiplyStr = (num1, num2) => {
+  // if any of our num is zero just return zero
+  if (num1 === "0" || num2 === "0") return "0";
+  // initiate array to keep track of current sums and their respective place
+  const sum = [];
+
+  // loop through our num strings and populate our sum array with updated sums and their respective place
+  for (let i = 0; i < num1.length; i++) {
+    for (let j = 0; j < num2.length; j++) {
+      if (!sum[i + j]) {
+        sum[i + j] = num1[i] * num2[j];
+      } else {
+        sum[i + j] += num1[i] * num2[j];
+      }
+    }
+  }
+  // reverse in place now ones and tens are in beginning of array
+  sum.reverse();
+
+  //loop through our temp sum array
+  for (let i = 0; i < sum.length; i++) {
+    // if our current number is higher than 9
+    if (sum[i] > 9) {
+      // and our next character is undefined, then this is the front of number
+      if (sum[i + 1] === undefined) {
+        // set next digit of return number as the higher digit of current sum
+        sum[i + 1] = Math.floor(sum[i] / 10);
+      } else {
+        // there is a number, add onto the digit, so it can carry over
+        sum[i + 1] += Math.floor(sum[i] / 10);
+      }
+      // modulo ten to get the current last digit
+      sum[i] = sum[i] % 10;
+    }
+  }
+  // return it back reversed and joined as a string
+  return sum.reverse().join("");
+};
