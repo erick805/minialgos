09e1a8ec97631f4a7f3f9c0429b27d375f642441
Ricky Lau <rickylaufitness@gmail.com>
2019-12-04 16:17:22 -0500

    add: linear solution to find min window wih target string in string

--- slidingWindow/minWindow.js
+++ slidingWindow/minWindow.js
@@ -6,3 +6,46 @@ Example:
 Input: S = "ADOBECODEBANC", T = "ABC"
 Output: "BANC"
 */
+
+const minWindow = (str, targetStr) => {
+  if (!str.length) return "";
+  // store all our keys and frequences from target string
+  const dict = {};
+  for (let letter of targetStr) {
+    dict[letter] = dict[letter] ? ++dict[letter] : 1;
+  }
+  let subStr = "";
+  let left = 0;
+  let right = -1;
+  let count = Object.keys(dict).length;
+
+  while (right < str.length) {
+    // shrink window because target string is within left and right
+    if (count === 0) {
+      // if there is no subString or current length is less than substring set new sub string
+      if (!subStr || right - left + 1 < subStr.length) {
+        subStr = str.slice(left, right + 1);
+      }
+      // get rid of current letter and then move left
+      if (dict[str[left]] !== undefined) {
+        dict[str[left]]++;
+      }
+
+      if (dict[str[left]] > 0) {
+        count++;
+      }
+      left++;
+    } else {
+      // expand window because target string is not within left and right
+      right++;
+      if (dict[str[right]] !== undefined) {
+        dict[str[right]]--;
+      }
+      if (dict[str[right]] === 0) {
+        count--;
+      }
+    }
+  }
+
+  return subStr;
+};
