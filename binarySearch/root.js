1364e9631a9963cdca13ab8051cea0f92d70d9e0
Ricky Lau <rickylaufitness@gmail.com>
2019-12-04 20:53:25 -0500

    add: find square root prompt and examples into binary search dir

---
+++ binarySearch/root.js
@@ -0,0 +1,13 @@
+/*
+Re-implement the square root function given a number x and a number n calculate the nth root of x within an error of 0.001
+
+i.e. suppose the real root is y, the solution must satisfy |y-root(x,n)| < 0.001.
+
+Examples:
+
+input:  x = 7, n = 3
+output: 1.913
+
+input:  x = 9, n = 2
+output: 3
+*/
