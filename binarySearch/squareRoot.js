1011ba23bf57340b789a246a4cb0ad842914a794
Ricky Lau <rickylaufitness@gmail.com>
2019-12-04 21:03:28 -0500

    add: find square root binary search pseudocode and solution

--- binarySearch/squareRoot.js
+++ binarySearch/squareRoot.js
@@ -11,3 +11,23 @@ output: 1.913
 input:  x = 9, n = 2
 output: 3
 */
+
+const squareRoot = (x, n) => {
+  // if our base is zero, the square root is zero
+  if (x === 0) return 0;
+  // intiate our lower and upperbounds
+  let lowerBound = 0;
+  // upperbound can't be less than one
+  let upperBound = Math.max(1, x);
+
+  let approxRoot = (upperBound + lowerBound) / 2;
+  // as long as their is a window - traverse
+  while (approxRoot - lowerBound >= 0.001) {
+    if (Math.pow(approxRoot, n) > x) upperBound = approxRoot;
+    else if (Math.pow(approxRoot, n) < x) lowerBound = approxRoot;
+    else break;
+    approxRoot = (upperBound + lowerBound) / 2;
+  }
+
+  return approxRoot;
+};
