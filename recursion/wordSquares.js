277906b4bba7afdf6b961f84f9e13dd5f5cf8d35
Ricky Lau <rickylaufitness@gmail.com>
2019-12-06 16:06:25 -0500

    add: trie class and recursive solution to word squares

--- recursion/wordSquares.js
+++ recursion/wordSquares.js
@@ -56,3 +56,77 @@ Output:
   ]
 ]
 */
+
+class Trie {
+  constructor() {
+    this.isWord = false;
+    this.children = new Map();
+  }
+
+  add(word) {
+    let cur = this;
+    for (let i = 0; i < word.length; i++) {
+      if (!cur.children.has(word[i])) {
+        cur.children.set(word[i], new Trie());
+      }
+      cur = cur.children.get(word[i]);
+    }
+    cur.isWord = true;
+  }
+
+  startWith(prefix) {
+    let cur = this;
+    for (let i = 0; i < prefix.length; i++) {
+      if (cur.children.has(prefix[i])) {
+        cur = cur.children.get(prefix[i]);
+      } else {
+        return [];
+      }
+    }
+
+    let res = [];
+    const findWords = (res, cur, str) => {
+      if (!cur.isWord) {
+        for (let [key, val] of cur.children) {
+          findWords(res, val, str + key);
+        }
+      } else {
+        res.push(str);
+      }
+    };
+    findWords(res, cur, prefix);
+    return res;
+  }
+}
+
+const wordSquares = words => {
+  const res = [];
+  const trie = new Trie();
+
+  for (let word of words) {
+    trie.add(word);
+  }
+
+  findWordSquare(res, [], trie);
+  return res;
+};
+
+const findWordSquare = (result, temp, trie) => {
+  if (temp.length > 0 && temp.length === temp[0].length) {
+    result.push(temp);
+    return;
+  }
+
+  let prefix = "";
+  let j = temp.length;
+
+  for (let i = 0; i < temp.length; i++) {
+    prefix += temp[i][j];
+  }
+
+  let startWith = trie.startWith(prefix);
+
+  for (let word of startWith) {
+    findWordSquare(result, temp.concat([word]), trie);
+  }
+};
