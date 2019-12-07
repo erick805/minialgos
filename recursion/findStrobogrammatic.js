bef3b1d3443d9797b6fdc93bf6dc8c4f835a1210
Ricky Lau <rickylaufitness@gmail.com>
2019-12-06 21:30:07 -0500

    add: find strobogrammatic recursive solution with helper function

--- recursion/findStrobogrammatic.js
+++ recursion/findStrobogrammatic.js
@@ -8,3 +8,27 @@ Example:
 Input: n = 2
 Output: ["11", "69", "88", "96"]
 */
+
+const findStrobogrammatic = n => {
+  return helper(n, n);
+};
+
+const helper = (n, m) => {
+  if (n === 0) return [""];
+  if (n === 1) return ["0", "1", "8"];
+
+  let list = helper(n - 2, m);
+  const res = [];
+  for (let i = 0; i < list.length; i++) {
+    const s = list[i];
+
+    if (n !== m) res.push("0" + s + "0");
+
+    res.push("1" + s + "1");
+    res.push("6" + s + "9");
+    res.push("8" + s + "8");
+    res.push("9" + s + "6");
+  }
+
+  return res;
+};
