fa0f4507a3831b97c247aa2a976f9763493575d8
Ricky Lau <rickylaufitness@gmail.com>
2019-12-07 14:55:11 -0500

    add: dfs recursive solution to word search

--- recursion/wordSearch.js
+++ recursion/wordSearch.js
@@ -14,3 +14,35 @@ Given word = "ABCCED", return true.
 Given word = "SEE", return true.
 Given word = "ABCB", return false.
 */
+
+const wordSearch = (board, word) => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === word[0] && dfs(board, i, j, 0, word)) return true;
+    }
+  }
+  return false;
+};
+
+const dfs = (board, i, j, count, word) => {
+  if (count === word.length) return true;
+
+  if (
+    i < 0 ||
+    i >= board.length ||
+    j < 0 ||
+    j >= board[i].length ||
+    board[i][j] !== word[count]
+  )
+    return false;
+  let temp = board[i][j];
+
+  board[i][j] = "";
+  const found =
+    dfs(board, i + 1, j, count + 1, word) ||
+    dfs(board, i - 1, j, count + 1, word) ||
+    dfs(board, i, j + 1, count + 1, word) ||
+    dfs(board, i, j - 1, count + 1, word);
+  board[i][j] = temp;
+  return found;
+};
