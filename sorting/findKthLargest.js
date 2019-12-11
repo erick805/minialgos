234bfb936f712e2faf0e952fc557152d452c489a
Ricky Lau <rickylaufitness@gmail.com>
2019-12-11 17:20:00 -0500

    add: quick select solution and pseudocode to find kth largest in arr

--- sorting/findKthLargest.js
+++ sorting/findKthLargest.js
@@ -14,3 +14,33 @@ Output: 4
 Note:
 You may assume k is always valid, 1 <= k <= array's length.
 */
+
+const findKthLargest = (nums, k) => {
+  return quickSelect(nums, 0, nums.length - 1, k);
+};
+
+const quickSelect = (arr, lo, hi, k) => {
+  let i = lo;
+  let j = lo;
+
+  while (j < hi) {
+    if (arr[j] <= arr[hi]) swap(arr, i++, j);
+    j++;
+  }
+
+  swap(nums, i, j);
+  // # of items greater than pivot point
+  const m = hi - i + 1;
+  // if we have same amount of items, we can return the value of pivot point
+  if (m === k) return arr[i];
+  // quick select right of pivot point
+  if (m > k) return quickSelect(arr, i + 1, hi, k);
+  // quick select left of pivot point
+  return quickSelect(nums, lo, i - 1, k - m);
+};
+
+const swap = (arr, i, j) => {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
