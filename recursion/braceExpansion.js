cb3644a56ffb07bd41a93b8b20922e82e0171433
Ricky Lau <rickylaufitness@gmail.com>
2019-12-14 13:04:41 -0500

    add: dfs and array chunk splitting solution for brace expansion

--- recursion/braceExpansion.js
+++ recursion/braceExpansion.js
@@ -20,3 +20,41 @@ Note:
 2. There are no nested curly brackets.
 3. All characters inside a pair of consecutive opening and ending curly brackets are different.
 */
+
+// Create sorted array of options and do a dfs on each option
+const expand = S => {
+  const options = createOptions(S);
+
+  return dfs(options);
+};
+
+const createOptions = optionStr => {
+  let start = 0;
+  const options = [];
+
+  for (let i = 0; i < optionStr.length; i++) {
+    if (optionStr[i] === "{" || optionStr[i] === "}") {
+      if (i > start) {
+        options.push(optionStr.substring(start, i));
+      }
+      start = i + 1;
+    }
+  }
+
+  if (start < optionStr.length) options.push(optionStr.substring(start));
+  // sort into alphabetical order
+  return options.map(str => str.split(",").sort());
+};
+
+const dfs = (arr, index = 0, selected = "", output = []) => {
+  if (index >= arr.length) {
+    output.push(selected);
+    return output;
+  }
+
+  for (const letter of arr[index]) {
+    dfs(arr, index + 1, selected + letter, output);
+  }
+
+  return output;
+};
