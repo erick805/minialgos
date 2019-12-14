412fc59ec18b88fd957e2dffbd87ef1ca3cc209e
Ricky Lau <rickylaufitness@gmail.com>
2019-12-14 12:50:09 -0500

    add: brace expansion prompt and examples into recursion dir

---
+++ recursion/braceExpansion.js
@@ -0,0 +1,22 @@
+/*
+A string s represents a list of words.
+
+Each letter in the word has 1 or more options. If there is one option, the letter is represented as is. If there is more than option, then curly braces delimit the options. For example, "{a,b,c}d{e,f}" represents the list ["ade", "adf", "bde", "bdf", "cde", "cdf"].
+
+Return all the words that can be formed in this manner, in lexicographical order.
+
+Example 1:
+Input: "{a,b}c{d,e}f"
+Output: ["acdf", "acef", "bcdf", "bcef"]
+
+Example 2:
+
+Input: "abcd"
+Output: ["abcd"]
+
+Note:
+
+1. 1 <= S.length <= 50
+2. There are no nested curly brackets.
+3. All characters inside a pair of consecutive opening and ending curly brackets are different.
+*/
