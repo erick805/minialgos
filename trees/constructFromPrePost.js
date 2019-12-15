2e67376e8193f2d5fa68d279b72d5e9c3afe0cd7
Ricky Lau <rickylaufitness@gmail.com>
2019-12-14 21:13:52 -0500

    add: dfs recursive solution and pseudocode to construct from pre and post traversal

--- trees/constructFromPrePost.js
+++ trees/constructFromPrePost.js
@@ -14,3 +14,28 @@ Note:
 - 1 <= pre.length == post.length <= 30
 - pre[] and post[] are both permutations of 1, 2, ...., pre.length
 */
+
+// pre = [1,2,4,5,3,6,7] (Root L R)
+// post = [4,5,2,6,7,3,1] (L R Root)
+
+const constructFromPrePost = (pre, post) => {
+  if (pre.length === 0) return null;
+  if (pre.length === 1) return new Node(pre[0]);
+
+  const root = new Node(pre[0]);
+  const left = pre[1];
+  // find root of left subtree
+  const leftIndex = post.indexOf(pre[1]);
+  // left subtree
+  root.left = constructFromPrePost(
+    pre.slice(1, leftIndex + 2),
+    post.slice(0, leftIndex + 1)
+  );
+  // right subtree
+  root.right = constructFromPrePost(
+    pre.slice(leftIndex + 2),
+    post.slice(leftIndex + 1)
+  );
+
+  return root;
+};
