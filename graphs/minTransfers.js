140b81cde5acc38cb0b2c65035db4dbb1c02b91a
Ricky Lau <rickylaufitness@gmail.com>
2019-12-17 19:39:56 -0500

    add: dfs + backtracking + hashmap solution to min transfers algo

--- graphs/minTransfers.js
+++ graphs/minTransfers.js
@@ -37,3 +37,42 @@ Person #2 gave person #0 $5.
 
 Therefore, person #1 only need to give person #0 $4, and all debt is settled.
 */
+
+// DFS + map of each person's account balance
+
+const minTransfers = transactions => {
+  // create hashmap of each person's account balance after transactions
+  const map = {};
+
+  for (const [p1, p2, delta] of transactions) {
+    map[p1] = (map[p1] || 0) - delta;
+    map[p2] = (map[p2] || 0) + delta;
+  }
+  // find all who are in debt or positve
+  const balances = Object.values(map).filter(val => val !== 0);
+  return dfs(balances);
+};
+
+const dfs = (balances, start = 0, minTrans = 0) => {
+  const unbalanced = findUnbalanced(balances, start);
+  if (unbalanced < 0) {
+    return minTrans;
+  }
+  let result = Infinity;
+  for (let i = unbalanced + 1; i < balances.length; i++) {
+    if (balances[unbalanced] * balances[i] > 0) continue;
+    balances[i] += balances[unbalanced];
+    result = Math.min(result, dfs(balances, unbalanced + 1, minTrans + 1));
+    balances[i] -= balances[unbalanced];
+  }
+  return result;
+};
+
+const findUnbalanced = (balances, start) => {
+  for (let i = start; i < balances.length; i++) {
+    if (balances[i] !== 0) {
+      return i;
+    }
+  }
+  return -1;
+};
