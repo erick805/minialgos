6bd0765b07b50b4331d027e8be3a6eff972e1e9a
Ricky Lau <rickylaufitness@gmail.com>
2019-12-18 16:55:07 -0500

    add: merge sort approach to count smaller algo

--- binarySearch/countSmaller.js
+++ binarySearch/countSmaller.js
@@ -55,3 +55,70 @@ const countSmaller = nums => {
 
   return count;
 };
+
+// Merge-sort approach
+
+const countSmaller = nums => {
+  if (nums.length === 0) return [];
+  if (nums.length === 1) return [0];
+
+  let items = nums.map(function(n, i) {
+    return { n, i, count: 0 };
+  });
+
+  const helper = items => {
+    const len = items.length;
+    const half = Math.floor(len / 2);
+
+    if (len <= 1) return items;
+
+    if (len === 2) {
+      if (items[1].n < items[0].n) {
+        items[0].count++;
+        const temp = items[1];
+        items[1] = items[0];
+        items[0] = temp;
+      }
+
+      return items;
+    }
+
+    let left = items.slice(0, half + 1);
+    let right = items.slice(half + 1);
+
+    (left = helper(left)), (right = helper(right));
+
+    const res = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+      let item1 = left[i];
+      let item2 = right[j];
+
+      if (item1.n <= item2.n) {
+        res.push(item1);
+        i++;
+        item1.count += j;
+      } else {
+        res.push(item2);
+        j++;
+      }
+    }
+
+    while (i < left.length) {
+      let item1 = left[i++];
+      res.push(item1);
+      item1.count += j;
+    }
+
+    while (j < right.length) {
+      res.push(right[j++]);
+    }
+    return res;
+  };
+  items = helper(items);
+  items.sort((a, b) => a.i - b.i);
+
+  return items.map(item => item.count);
+};
