a9dd067d325dd86bc71cc978d2d5b0b7121eba1e
Ricky Lau <rickylaufitness@gmail.com>
2019-12-18 16:11:29 -0500

    add: BST approach to count smaller algo

--- binarySearch/countSmaller.js
+++ binarySearch/countSmaller.js
@@ -11,3 +11,47 @@ Example:
   To the right of 6 there is only 1 smaller element (1).
   To the right of 1 there is 0 smaller element.
 */
+
+// BST approach
+class BST {
+  constructor(v, s = 0) {
+    this.value = v;
+    this.left = null;
+    this.right = null;
+    this.size = s;
+  }
+
+  insert(val, s = 0) {
+    if (val > this.value) {
+      if (this.right) {
+        return this.right.insert(val, s + this.size + 1); // increment one because we care about greater
+      } else {
+        this.right = new BST(val);
+        return this.size + s + 1;
+      }
+    } else {
+      this.size++;
+      if (this.left) {
+        return this.left.insert(val, s);
+      } else {
+        this.left = new BST(val);
+        return s;
+      }
+    }
+  }
+}
+
+const countSmaller = nums => {
+  const bst = new BST(nums[nums.length - 1]);
+
+  const count = new Array(nums.length);
+
+  count[count.length - 1] = 0;
+
+  // right -> left means greater
+  for (let i = nums.length - 2; i >= 0; i--) {
+    count[i] = bst.insert(nums[i]);
+  }
+
+  return count;
+};
