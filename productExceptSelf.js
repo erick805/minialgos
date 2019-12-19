c82c99f95aa6217af1eaaa979bd750a72d6fc8e9
Ricky Lau <rickylaufitness@gmail.com>
2019-12-19 15:09:30 -0500

    add: linear and optimized space solution to product except self algo

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -12,3 +12,47 @@ Follow up:
 
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 */
+
+// Time Complexity: O(n)
+
+const productExceptSelf = nums => {
+  const leftProd = [];
+  const rightProd = [];
+  const output = [];
+
+  (leftProd[0] = 1), (rightProd[nums.length - 1] = 1);
+
+  // get all products left of self
+  for (let i = 1; i < nums.length; i++) {
+    leftProd[i] = nums[i - 1] * leftProd[i - 1];
+  }
+  // get all products right of self
+  for (let i = nums.length - 2; i >= 0; i--) {
+    rightProd[i] = nums[i + 1] * rightProd[i + 1];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    output[i] = leftProd[i] * rightProd[i];
+  }
+
+  return output;
+};
+
+// Optimization - O(1) space
+
+const productExceptSelf = nums => {
+  const output = [1];
+
+  // get all products left of self
+  for (let i = 1; i < nums.length; i++) {
+    output[i] = output[i - 1] * nums[i - 1];
+  }
+
+  let R = 1; // initiate variable to keep track of products to right
+  for (let i = nums.length - 1; i >= 0; i--) {
+    output[i] = output[i] * R;
+    R = nums[i] * R; // accumulate variable
+  }
+
+  return output;
+};
