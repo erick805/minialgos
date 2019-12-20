8c25ec79e2b9d840baf6562b98a02df22a9dbc74
Ricky Lau <rickylaufitness@gmail.com>
2019-12-20 14:23:02 -0500

    add: two pointer & sliding window solution and pseudocode to min window subsequence

--- slidingWindow/minWindowSeq.js
+++ slidingWindow/minWindowSeq.js
@@ -17,3 +17,41 @@ Note:
   2. The length of S will be in the range [1, 2000].
   3. The length of T will be in the range [1, 100].
 */
+
+// 2 pointers + sliding window
+
+const minWindow = (S, T) => {
+  let sIdx = 0;
+  let tIdx = 0;
+  const s = S.length;
+  const t = T.length;
+
+  let subStr = "";
+  // iterate through entire string
+  while (sIdx < s) {
+    if (S[sIdx] === T[tIdx]) {
+      tIdx++;
+      if (tIdx === t) {
+        const end = sIdx + 1; // capture end of last matched index
+        tIdx--; // move pointer back to last matched char
+
+        // reset t and s to beg
+        while (tIdx >= 0) {
+          if (S[sIdx] === T[tIdx]) tIdx--;
+          sIdx--;
+        }
+        sIdx++; // reset s index back to first matching index
+
+        tIdx++; // reset t index back to 0
+
+        // reassign shortest sub string
+        if (!subStr || end - sIdx < subStr.length) {
+          subStr = S.substring(sIdx, end);
+        }
+      }
+    }
+    sIdx++; // iterate to next letter
+  }
+
+  return !subStr ? "" : subStr;
+};
