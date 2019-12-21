97c5aff62bf6b86688df13d18b3c41b6e0379aae
Ricky Lau <rickylaufitness@gmail.com>
2019-12-21 11:12:24 -0500

    add: priority queue approach to merge k sorted lists algo

--- sorting/mergeKLists.js
+++ sorting/mergeKLists.js
@@ -13,4 +13,106 @@ Example:
 
 
   Output: 1->1->2->3->4->4->5->6
+
+function ListNode(val) {
+  this.val = val
+  this.next = null
+}
 */
+
+// PriorityQueue
+const mergeKLists = lists => {
+  const pq = new PriorityQueue();
+  lists.forEach(list => {
+    if (list) pq.enqueue(list, list.val);
+  });
+
+  const res = new ListNode(null);
+
+  let cur = res;
+
+  while (!pq.isEmpty()) {
+    cur.next = pq.dequeue();
+    cur = cur.next;
+    if (cur.next) pq.enqueue(cur.next, cur.next.val);
+  }
+
+  return res.next;
+};
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    const node = new Node(val, priority);
+
+    this.values.push(node);
+    this.bubbleUp();
+  }
+
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+
+    if (this.values.length) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+
+    return min.val;
+  }
+
+  isEmpty() {
+    return !this.values.length;
+  }
+
+  bubbleUp(index = this.values.length - 1) {
+    if (index <= 0) return;
+    const parentIdx = Math.floor((index - 1) / 2);
+
+    if (this.values[index].priority <= this.values[parentIdx].priority) {
+      [this.values[index], this.values[parentIdx]] = [
+        this.values[parentIdx],
+        this.values[index]
+      ];
+      this.bubbleUp(parentIdx);
+    }
+  }
+
+  bubbleDown(index = 0, swapIdx = null) {
+    let leftIdx = index * 2 + 1,
+      rightIdx = index * 2 + 2,
+      length = this.values.length;
+
+    if (leftIdx < length) {
+      if (this.values[leftIdx].priority <= this.values[index].priority)
+        swapIdx = leftIdx;
+    }
+
+    if (rightIdx < length) {
+      if (
+        (swapIdx === null &&
+          this.values[rightIdx].priority <= this.values[index].priority) ||
+        (swapIdx !== null &&
+          this.values[rightIdx].priority < this.values[leftIdx].priority)
+      )
+        swapIdx = rightIdx;
+    }
+
+    if (swapIdx !== null) {
+      [this.values[index], this.values[swapIdx]] = [
+        this.values[swapIdx],
+        this.values[index]
+      ];
+      this.bubbleDown(swapIdx, null);
+    }
+  }
+}
