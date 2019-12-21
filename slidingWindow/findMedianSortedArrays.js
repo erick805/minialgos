b1385b1334533c6ba4b7122a3c8f376917e87621
Ricky Lau <rickylaufitness@gmail.com>
2019-12-20 20:46:10 -0500

    add: partition binary search solution and pseudocode to find median sorted arrays algo

--- slidingWindow/findMedianSortedArrays.js
+++ slidingWindow/findMedianSortedArrays.js
@@ -19,3 +19,35 @@ Example 2:
 
   The median is (2 + 3) / 2 = 2.5
 */
+
+const findMedianSortedArrays = (nums1, nums2) => {
+  // perform on shorter of two arrays
+  if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
+  const x = nums1.length;
+  const y = nums2.length;
+
+  let lo = 0;
+  let hi = x;
+
+  while (lo <= hi) {
+    let partitionX = ((lo + hi) / 2) | 0;
+    let partitionY = ((x + y + 1) / 2 - partitionX) | 0;
+
+    let maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1];
+    let minRightX = partitionX === x ? Infinity : nums1[partitionX];
+
+    let maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1];
+    let minRightY = partitionY === y ? Infinity : nums2[partitionY];
+
+    if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+      if ((x + y) & 1) return Math.max(maxLeftX, maxLeftY); // odd length
+      return (
+        (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2
+      ); // even length
+
+      // binary search - move closer left
+    } else if (maxLeftX > minRightY) hi = partitionX - 1;
+    else lo = partitionX + 1; // move closer right
+  }
+};
