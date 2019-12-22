ddbfb48394bb007cbef181c68c1062c3fd2f75a2
Ricky Lau <rickylaufitness@gmail.com>
2019-12-21 20:18:55 -0500

    add: binary search solution to maximize sweetness algo

--- binarySearch/maximizeSweetness.js
+++ binarySearch/maximizeSweetness.js
@@ -26,3 +26,38 @@ Constraints:
   1. 0 <= K <= sweetness.length <= 10^4
   2. 1 <= sweetness[i] <= 10^5
 */
+
+const maximizeSweetness = (sweetness, K) => {
+  let min = Math.min(...sweetness); // n chocolate bars
+  let max = sweetness.reduce((acc, e) => acc + e, 0); // 1 chocolate bar
+
+  if (K + 1 === 1) return max;
+  if (K + 1 === sweetness.length) return min;
+
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+
+    const pieces = getPieces(sweetness, mid);
+    // include yourself
+    if (pieces > K + 1) {
+      min = mid + 1; // move closer to max - reduce pieces
+    } else {
+      max = mid - 1; // move closer to min - increase pieces
+    }
+  }
+  return min;
+};
+
+const getPieces = (arr, t) => {
+  let pieces = 1;
+  let total = 0;
+
+  for (const n of arr) {
+    total += n;
+    if (total > t) {
+      pieces++;
+      total = 0;
+    }
+  }
+  return pieces;
+};
