7b1136a6b2521de3cfa6f80d147aa2c6da203036
Ricky Lau <rickylaufitness@gmail.com>
2019-12-24 09:43:01 -0500

    add: minimum meeting rooms solution into main dir

--- minMeetingRooms.js
+++ minMeetingRooms.js
@@ -11,3 +11,24 @@ Example 2:
   Input: [[7,10],[2,4]]
   Output: 1
 */
+
+const minMeetingRooms = intervals => {
+  if (!intervals) return 0;
+
+  const starts = intervals.map(a => a[0]).sort((a, b) => a - b);
+  const ends = intervals.map(a => a[1]).sort((a, b) => a - b);
+
+  let rooms = 0;
+  let end = 0;
+
+  for (let i = 0; i < intervals.length; i++) {
+    // if current start time is less than - we need another room
+    if (starts[i] < ends[end]) {
+      rooms++;
+    } else {
+      end++; // extend from current end
+    }
+  }
+
+  return rooms;
+};
