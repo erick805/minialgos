4c30af064789319c2a1a4d7ce0d48e237a9fc3c6
Ricky Lau <48040084+rickylaufitness@users.noreply.github.com>
2020-01-03 16:04:10 -0500

    Update productSum.py

--- recursion/productSum.py
+++ recursion/productSum.py
@@ -1,8 +1,8 @@
 '''
-Write a function that takes in a "special" array and returns its product sum.\n 
-A "special" array is a non-empty array that contains either integers or other "special" arrays.\n 
-The product sum of a "special" array is the sum of its elements, where "special" arrays inside\n 
-it should be summed themselves and then multiplied by their level of depth. \n
+Write a function that takes in a "special" array and returns its product sum. 
+A "special" array is a non-empty array that contains either integers or other "special" arrays. 
+The product sum of a "special" array is the sum of its elements, where "special" arrays inside 
+it should be summed themselves and then multiplied by their level of depth.
 For example, the product sum of [x,y] is x + y; the product sum of [x[y,z]] = x + 2y + 2z.
 
   Input: [5,2[7,-1],3,[6,[-13,8],4]]
