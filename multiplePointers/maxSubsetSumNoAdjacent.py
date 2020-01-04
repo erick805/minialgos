022f6e7b448a2f9c8eed47a26f2b9d6a20d9bb01
Ricky Lau <rickylaufitness@gmail.com>
2020-01-04 15:28:54 -0500

    add: multiple pointers solution to max sub set sum no adjacent

--- multiplePointers/maxSubsetSumNoAdjacent.py
+++ multiplePointers/maxSubsetSumNoAdjacent.py
@@ -4,3 +4,19 @@ Write a function that takes in an array of positive integers and returns an inte
 Input: [75,105,120,75,90,135]
 Output: 330 (75 + 120 + 135)
 '''
+
+
+def maxSubsetSumNoAdjacent(array):
+    if not len(array):
+        return 0
+    elif len(array) == 1:
+        return array[0]
+
+    second = array[0]
+    first = max(array[0], array[1])
+
+    for i in range(2, len(array)):
+        current = max(first, second + array[i])
+        second = first
+        first = current
+    return first
