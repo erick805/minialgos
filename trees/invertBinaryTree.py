62fba1ff42bf8bd7ffa490240c1973684eb5aaee
Ricky Lau <rickylaufitness@gmail.com>
2020-01-04 13:18:42 -0500

    add: dfs recursive solution with time and space to invert binary tree algo

--- trees/invertBinaryTree.py
+++ trees/invertBinaryTree.py
@@ -37,3 +37,17 @@ def invertBinaryTree(tree):
 
 def swapLeftAndRight(tree):
     tree.left, tree.right = tree.right, tree.left
+
+# dfs recursive - O(n) time | O(d) space, d = depth of tree
+
+
+def invertBinaryTree(tree):
+    if tree is None:
+        return
+    swapLeftAndRight(tree)
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
