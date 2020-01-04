28fc000ee00de993f3c32be806d195c026df2ebe
Ricky Lau <rickylaufitness@gmail.com>
2020-01-04 17:07:06 -0500

    create-add: dynamic programming dir and number of ways to make change prompt and example

---
+++ dp/numberOfWaysToMakeChange.py
@@ -0,0 +1,9 @@
+'''
+Given an array of positive integers representing coin denominations and a single non-negative integer
+representing a target amount of money, implement a function that returns the number of ways to make
+change for that target amount using the given coin denominations. Note that an unlimited amount of
+coins is at your disposal.
+
+Input: 6, [1,5]
+Output: 2 (1*1 + 1*5 and 6*1)
+'''
