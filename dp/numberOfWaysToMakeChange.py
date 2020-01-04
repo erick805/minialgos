1321a2475a6078f20ad9fe87598731ce5d54c599
Ricky Lau <rickylaufitness@gmail.com>
2020-01-04 17:10:38 -0500

    add: dynamic programming solution to number of ways to make change algo

--- dp/numberOfWaysToMakeChange.py
+++ dp/numberOfWaysToMakeChange.py
@@ -7,3 +7,15 @@ coins is at your disposal.
 Input: 6, [1,5]
 Output: 2 (1*1 + 1*5 and 6*1)
 '''
+
+
+def numberOfWaysToMakeChange(n, denoms):
+    ways = [0 for amount in range(n + 1)]
+    ways[0] = 1
+
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if denom <= amount:
+                ways[amount] += ways[amount - denom]
+
+    return ways[n]
