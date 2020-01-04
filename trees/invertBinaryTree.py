c7c4c9106ee936580f9d05e351ab8032b0ed43ed
Ricky Lau <rickylaufitness@gmail.com>
2020-01-04 13:04:49 -0500

    add: bfs iterative solution, time and space complexity to invert binary tree algo

--- trees/invertBinaryTree.py
+++ trees/invertBinaryTree.py
@@ -20,3 +20,20 @@ Output: 1
             / \
            9   8
 '''
+
+# bfs iterative - O(n) time | O(n) space
+
+
+def invertBinaryTree(tree):
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftAndRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
+
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
