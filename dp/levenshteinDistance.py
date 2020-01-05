72baab87dac9ef8df1ffd7ad4542e3176560998e
Ricky Lau <rickylaufitness@gmail.com>
2020-01-05 14:52:36 -0500

    add: dp matrix solution to levenshtein distance algo

--- dp/levenshteinDistance.py
+++ dp/levenshteinDistance.py
@@ -6,3 +6,21 @@ substitution of a character for another.
 Input: str1 = "abc", str2 = "yabd"
 Output: 2 (insert "y", substitute "c" for "d")
 '''
+
+
+def levenshteinDistance(str1, str2):
+    # build matrix
+    edits = [[x for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+
+    for i in range(1, len(str2) + 1):
+        edits[i][0] = edits[i - 1][0] + 1
+
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i - 1] == str1[j - 1]:
+                edits[i][j] = edits[i - 1][j - 1]
+            else:
+                edits[i][j] = 1 + min(edits[i - 1][j - 1],
+                                      edits[i - 1][j], edits[i][j - 1])
+
+    return edits[-1][-1]  # return last value in matrix
