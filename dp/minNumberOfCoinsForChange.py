5484f0086cfa8daa44cc04b02e807adceeb0572c
Ricky Lau <rickylaufitness@gmail.com>
2020-01-05 12:16:52 -0500

    add: min number of coins for change prompt and example into dp dir

---
+++ dp/minNumberOfCoinsForChange.py
@@ -0,0 +1,9 @@
+'''
+Given an array of positive integers representing the coin denominations and a single non-negative integer representing a target
+amount of money, implement a function that returns the smallest number of coins needed to make change for that target amount using
+the given coin denominations. Note that an unlimited amount of coins is at your disposal. If it is impossible to make change for the
+target amount, return -1.
+
+Input: n = 7, denoms = [1,5,10]
+Output 3 (2*1 + 1*5)
+'''
