d97132dc0e9233460f5d5a83eee787d3f03e9780
Ricky Lau <rickylaufitness@gmail.com>
2020-01-10 10:11:53 -0500

    add: breadth first search tree prompt and example into graphs dir

---
+++ graphs/bfsTree.py
@@ -0,0 +1,16 @@
+'''
+You are given a Node class that has a name and an array of optional children Nodes. When put together, Nodes form a
+simple tree-like structure. Implement the breadthFirstSearch method on the Node class, which takes in an empty array,
+traverses the tree using the Breadth-first Search approach(specifically navigating the tree from left to right),
+stores all of the Node's names in the input array, and returns it.
+
+Input:   A
+       / | \
+      B  C  D
+    /  \   / \
+   E   F  G   H
+      / \  \
+     I   J  K
+
+Output: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+'''
