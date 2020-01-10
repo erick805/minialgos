3fcd9755e93e2562f17f1b4accb92be94d0b8177
Ricky Lau <rickylaufitness@gmail.com>
2020-01-10 10:20:56 -0500

    add: breadth first search solution to bfs tree algo

--- graphs/bfsTree.py
+++ graphs/bfsTree.py
@@ -24,3 +24,12 @@ class Node:
     def addChild(self, name):
         self.children.append(Node(name))
         return self
+
+    def breadthFirstSearch(self, array):
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            array.append(current.name)
+            for child in current.children:
+                queue.append(child)
+        return array
