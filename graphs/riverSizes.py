daf2815b34c80670a4c51c99ef80fc24ea604b2b
Ricky Lau <rickylaufitness@gmail.com>
2020-01-10 14:13:49 -0500

    add: initialize river sizes with visited matrix and for loops

--- graphs/riverSizes.py
+++ graphs/riverSizes.py
@@ -16,3 +16,14 @@ Input:
 
 Output: [1,2,2,2,5]
 '''
+
+
+def riverSizes(matrix):
+    sizes = []
+    visited = [[False for value in row] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if visited[i][j]:
+                continue
+            traverseNode(i, j, matrix, visited, sizes)
+    return sizes
