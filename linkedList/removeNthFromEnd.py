9aa9046009a932006757172f1168ed81e383a936
Ricky Lau <rickylaufitness@gmail.com>
2020-01-11 15:41:45 -0500

    add: remove nth from end of singly ll solution

--- linkedList/removeNthFromEnd.py
+++ linkedList/removeNthFromEnd.py
@@ -13,3 +13,24 @@ class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+def removeNthFromEnd(head, n):
+    first = head
+    second = head
+    counter = 0
+
+    while counter < n:
+        second = second.next
+        counter += 1
+
+    if second is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+
+    while second.next is not None:
+        second = second.next
+        first = first.next
+
+    first.next = first.next.next
