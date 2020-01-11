f71e059a427142890bcb833df85225b743ca33ad
Ricky Lau <rickylaufitness@gmail.com>
2020-01-11 17:39:06 -0500

    add: integer permutations python prompt and example into main dir

---
+++ permutations.py
@@ -0,0 +1,7 @@
+'''
+Write a function that takes in an array of unique integers and returns an array of all permutations of those integers.
+If the input array is empty, your function should return an empty array.
+
+Input: array = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+'''
