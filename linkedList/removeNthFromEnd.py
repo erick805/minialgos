9527dd56443bb24ad80821f8c02140be6ab62e4c
Ricky Lau <rickylaufitness@gmail.com>
2020-01-11 15:37:10 -0500

    add: linked list python class

--- linkedList/removeNthFromEnd.py
+++ linkedList/removeNthFromEnd.py
@@ -7,3 +7,9 @@ if it is the tail of the list.
 Input: head = 0->1->2->3->4->5->6->7->8->9, n = 4
 Output: head = 0->1->2->3->4->5->7->8->9
 '''
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
