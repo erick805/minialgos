060835756fbf31a32ceae1a63e2b07a686b62733
Ricky Lau <rickylaufitness@gmail.com>
2020-01-12 17:55:18 -0500

    add: time and space complexity to min max stack class methods

--- dataStructures/MinMaxStack.py
+++ dataStructures/MinMaxStack.py
@@ -43,19 +43,24 @@ class MinMaxStack:
         self.minMaxStack = []
         self.stack = []
 
+    # O(1) time | O(1) space
     def getMin(self):
         return self.minMaxStack[len(self.minMaxStack) - 1]["min"]
 
+    # O(1) time | O(1) space
     def getMax(self):
         return self.minMaxStack[len(self.minMaxStack) - 1]["max"]
 
+    # O(1) time | O(1) space
     def peek(self):
         return self.stack[len(self.stack) - 1]
 
+    # O(1) time | O(1) space
     def pop(self):
         self.minMaxStack.pop()
         return self.stack.pop()
 
+    # O(1) time | O(1) space
     def push(self, number):
         newMinMax = {"min": number, "max": number}
         if len(self.minMaxStack):
