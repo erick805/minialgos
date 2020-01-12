098b929bd5dd4f606ab810edb241053a30d826e5
Ricky Lau <rickylaufitness@gmail.com>
2020-01-12 17:53:38 -0500

    add: min max stack class solution

--- dataStructures/MinMaxStack.py
+++ dataStructures/MinMaxStack.py
@@ -36,3 +36,32 @@ Output:
 5(max)
 5(peek)
 '''
+
+
+class MinMaxStack:
+    def __init__(self):
+        self.minMaxStack = []
+        self.stack = []
+
+    def getMin(self):
+        return self.minMaxStack[len(self.minMaxStack) - 1]["min"]
+
+    def getMax(self):
+        return self.minMaxStack[len(self.minMaxStack) - 1]["max"]
+
+    def peek(self):
+        return self.stack[len(self.stack) - 1]
+
+    def pop(self):
+        self.minMaxStack.pop()
+        return self.stack.pop()
+
+    def push(self, number):
+        newMinMax = {"min": number, "max": number}
+        if len(self.minMaxStack):
+            lastMinMax = self.minMaxStack[len(self.minMaxStack) - 1]
+            newMinMax["min"] = min(lastMinMax["min"], number)
+            newMinMax["max"] = max(lastMinMax["max"], number)
+
+        self.minMaxStack.append(newMinMax)
+        self.stack.append(number)
