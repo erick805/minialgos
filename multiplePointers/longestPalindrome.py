62ed5d1a02f3b5f31dbc2eb2d1e070d3abe09ede
Ricky Lau <rickylaufitness@gmail.com>
2020-01-13 09:25:57 -0500

    add: longest palindrome python main function solution

--- multiplePointers/longestPalindrome.py
+++ multiplePointers/longestPalindrome.py
@@ -5,3 +5,20 @@ that is written the same forward and backward. Assume that there will only be on
 Input: "abaxyzzyxf
 Output: "xyzzyx"
 '''
+
+
+def longestPalindrome(string):
+    currentLongest = [0, 1]
+
+    for i in range(1, len(string)):
+        odd = getLongestPalindrome(
+            string, i - 1, i + 1)  # with letter as center
+
+        # without letter as center
+        even = getLongestPalindrome(string, i - 1, i)
+
+        longest = max(odd, even, key=lambda x: x[1] - x[0])
+        currentLongest = max(longest, currentLongest,
+                             key=lambda x: x[1] - x[0])
+
+    return string[currentLongest[0]: currentLongest[1]]
