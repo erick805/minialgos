b6cff055dfc99a8c5d91408927a9cf5b32cec70e
Ricky Lau <rickylaufitness@gmail.com>
2020-01-13 13:11:35 -0500

    add: group anagrams hashtable solution and prompt

---
+++ groupAnagrams.py
@@ -0,0 +1,21 @@
+'''
+Write a function that takes in an array of strings and returns a list of group of anagrams. Anagrams are strings made
+up of exactly the same letters, where order doesn't matter. For example, "cinema" and "iceman" are anagrams; similarly,
+"foo" and "ofo" are anagrams. Note that the groups of anagrams don't need to be ordered in any particular way.
+
+Input: ["yo","act","flop","tac","cat","oy","olfp"]
+Output: [["yo", "oy"], ["act", "tac", "cat"], ["flop", "olfp"]]
+'''
+
+
+def groupAnagrams(words):
+    anagrams = {}
+
+    for word in words:
+        sortedWord = "".join(sorted(word))
+        if sortedWord in anagrams:
+            anagrams[sortedWord].append(word)
+        else:
+            anagrams[sortedWord] = [word]
+
+    return list(anagrams.values())
