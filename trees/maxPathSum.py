146ec3858c63b6a538168e91275b270a883e18a9
Ricky Lau <rickylaufitness@gmail.com>
2020-01-14 10:48:44 -0500

    add: max path sum python prompt and example into trees dir

---
+++ trees/maxPathSum.py
@@ -0,0 +1,15 @@
+'''
+Write a function that takes in a binary tree and returns its max path sum. A path is a collection of connected nodes
+where no node is connected to more than two other nodes; a path sum is the sum of the values of the nodes in a
+particular path. Each binary tree node has a value stored in a property called "value" and two children nodes stored
+in properties called "left" and "right", respectively. Children nodes can either be binary tree nodes themselves or
+the None(null) value.
+
+Input:  1
+       / \
+      2   3
+     / \  / \
+    4  5 6   7
+
+Output: 18
+'''
