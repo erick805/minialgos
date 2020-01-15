de1b9eae9d1dae5fa1f8870a5ac3cd88f3b81a81
Ricky Lau <rickylaufitness@gmail.com>
2020-01-15 08:48:07 -0500

    add: four number sums solution

--- multiplePointers/fourNumberSum.py
+++ multiplePointers/fourNumberSum.py
@@ -7,3 +7,25 @@ should return an empty array.
 Input: array = [7,6,4,-1,1,2], targetSum = 16
 Output: [[7,6,4,-1],[7,6,1,2]]
 '''
+
+
+def fourNumberSum(array, targetSum):
+    allPairSums = {}
+    quadruplets = []
+
+    for i in range(1, len(array) - 1):
+        for j in range(i + 1, len(array)):
+            currentSum = array[i] + array[j]
+            diff = targetSum - currentSum
+
+            if diff in allPairSums:
+                for pair in allPairSums[diff]:
+                    quadruplets.append(pair + [array[i], array[j]])
+        for k in range(0, i):
+            currentSum = array[i] + array[k]
+            if currentSum not in allPairSums:
+                allPairSums[currentSum] = [[array[k], array[i]]]
+            else:
+                allPairSums[currentSum].append([array[k], array[i]])
+
+    return quadruplets
