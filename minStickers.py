05a76cffbd77dc29c503c9a8662a28f56c84c103
Ricky Lau <rickylaufitness@gmail.com>
2020-01-31 18:29:34 -0500

    create: minimum stickers prompt and example into main dir

---
+++ minStickers.py
@@ -0,0 +1,18 @@
+'''
+You are a children's soccer coach and you have a list of student's with values of unique goals scored.
+No goal scored is the same and order is not necessarily sorted. You want to reward each student with sticker's
+for each respective amount of goals scored.
+
+There are only three rules:
+- each student must receive at least one sticker
+- any given student must receive strictly more stickers than adjacent(to the left or
+to the right) student's with a lower score.
+- any given student must receive strictly fewer stickers than adjacent student's with
+a higher score.
+
+Write a function that takes in a list of goals scored and returns the minimum number of stickers that you must
+give out to all the students, while satisfying the three rules.
+
+Input: [8,4,2,1,3,6,7,9,5]
+Output: 25 ([4,3,2,1,2,3,4,5,1])
+'''
