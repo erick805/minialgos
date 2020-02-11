a7cb07e5f0651b89794554fe424a79512b67d538
Ricky Lau <rickylaufitness@gmail.com>
2020-02-11 14:36:25 -0500

    create-add: zigZagTraverse prompt and example into graphs dir

---
+++ graphs/zigZagTraverse.py
@@ -0,0 +1,16 @@
+'''
+Write a traversal function that prints a one-dimensional array of all the numbeers within the
+given matrix. The matrix is a two dimensional array with array's as child elements of the matrix.
+Assuming the zig zag order starts at the top left corner of the two-dimensional array, goes down
+by one element, and proceeds to traverse in a zig zag pattern all the way to the bottom right corner.
+
+Input: matrix =
+[
+  [1,3,4,10],
+  [2,5,9,11],
+  [6,8,12,15],
+  [7,13,14,16]
+]
+
+Output: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+'''
