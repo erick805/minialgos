77574367148ab0cd3a432b96b16ec1f337fb263c
Ricky Lau <rickylaufitness@gmail.com>
2020-02-11 17:55:38 -0500

    add: max sum increasing sub sequence dp solution

--- dp/maxSumIncSubSequence.py
+++ dp/maxSumIncSubSequence.py
@@ -8,3 +8,29 @@ same order as they appear in our input array.
 Input: array = [10,20,50,70,30,40]
 Output: [130, [10,50,70]]
 '''
+
+
+def maxSumIncSubSequence(array):
+    sequences = [None for num in array]
+    sums = array[:]
+    maxSumIdx = 0
+
+    for i in range(len(array)):
+        currentNum = array[i]
+        for j in range(0, i):
+            otherNum = array[j]
+            if otherNum < currentNum and sums[j] + currentNum >= sums[i]:
+                sums[i] = sums[j] + currentNum
+                sequences[i] = j
+        if sums[i] >= sums[maxSumIdx]:
+            maxSumIdx = i
+
+    return [sums[maxSumIdx], buildIncSequence(array, sequences, maxSumIdx)]
+
+
+def buildIncSequence(array, sequences, currentIdx):
+    sequence = []
+    while currentIdx is not None:
+        sequence.append(array[currentIdx])
+        currentIdx = sequences[currentIdx]
+    return list(reversed(sequence))
