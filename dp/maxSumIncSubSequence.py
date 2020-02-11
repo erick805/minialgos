852bd39b2b484ab44b7c216c481c10d013849f3e
Ricky Lau <rickylaufitness@gmail.com>
2020-02-11 17:44:53 -0500

    create-add: maxSumIncSubSequence prompt and example into dp dir

---
+++ dp/maxSumIncSubSequence.py
@@ -0,0 +1,10 @@
+'''
+Given an non-empty array of integers, write a function that returns an array of two elements.
+The first element in the return array is the maximum strictly increasing sub sequence sum within our
+input. The second element in the return array is the increasing sub sequence that sums up to the first
+element. A sub sequence is defined as a set of numbers that are not neccesarily adjacent but are in the
+same order as they appear in our input array.
+
+Input: array = [10,20,50,70,30,40]
+Output: [130, [10,50,70]]
+'''
