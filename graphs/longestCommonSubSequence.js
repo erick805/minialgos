e7cbb5777b9bf4b259709c409b1549f99eaa9dc5
Ricky Lau <rickylaufitness@gmail.com>
2020-02-29 17:49:41 -0500

    add: longestCommonSubSequence js solution

--- graphs/longestCommonSubSequence.js
+++ graphs/longestCommonSubSequence.js
@@ -8,3 +8,47 @@ Assume there is only one longest common subsequence.
 Input: str1 = "AMBHCD", str2 = "ABYCMMD"
 Output: ["A", "B", "C", "D"]
 */
+
+const longestCommonSubSequence = (str1, str2) => {
+  const lcs = [];
+  for (let i = 0; i < str2.length + 1; i++) {
+    const row = [];
+    for (let j = 0; j < str1.length + 1; j++) {
+      const entry = new Array(4);
+      entry[1] = 0;
+      row.push(entry);
+    }
+    lcs.push(row);
+  }
+
+  for (let r = 1; r < str2.length + 1; r++) {
+    for (let c = 1; c < str1.length + 1; c++) {
+      if (str2[r - 1] === str1[c - 1]) {
+        lcs[r][c] = [str2[r - 1], lcs[r - 1][c - 1][1] + 1, r - 1, c - 1];
+      } else {
+        if (lcs[r - 1][c][1] > lcs[r][c - 1][1]) {
+          lcs[r][c] = [null, lcs[r - 1][c][1], r - 1, c];
+        } else {
+          lcs[r][c] = [null, lcs[r][c - 1][1], r, c - 1];
+        }
+      }
+    }
+  }
+  return buildSequence(lcs);
+};
+
+const buildSequence = lcs => {
+  const sequence = [];
+  let i = lcs.length - 1;
+  let j = lcs[0].length - 1;
+
+  while (i !== 0 && j !== 0) {
+    let current = lcs[i][j];
+    if (current[0]) {
+      sequence.unshift(current[0]);
+    }
+    i = current[2];
+    j = current[3];
+  }
+  return sequence;
+};
