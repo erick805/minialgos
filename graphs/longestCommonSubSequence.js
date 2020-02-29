a221a600e299f00ef668e0070305dcf5f023902b
Ricky Lau <rickylaufitness@gmail.com>
2020-02-29 17:45:24 -0500

    create-add: longest common subsequence prompt and example

---
+++ graphs/longestCommonSubSequence.js
@@ -0,0 +1,10 @@
+/*
+Implement a function that takes in two strings of arbitary length and return the longest common subsequence
+based off the two strings. A subsequence is defined as a group of values or in this case characters that appear
+sequentially after one another with no importance to their actual position in the string. In other words,
+characters do not have to appear consecutively in order to form a sequence.
+Assume there is only one longest common subsequence.
+
+Input: str1 = "AMBHCD", str2 = "ABYCMMD"
+Output: ["A", "B", "C", "D"]
+*/
