61cd80ef4e419d69ae32f68b4f49e96a7da2b93e
Ricky Lau <rickylaufitness@gmail.com>
2020-03-02 21:51:20 -0500

    add: add logarithmic solution to find median sorted arrays python algo

--- slidingWindow/findMedianSortedArrays.py
+++ slidingWindow/findMedianSortedArrays.py
@@ -19,3 +19,36 @@ Example 2:
 
   The median is (2 + 3) / 2 = 2.5
 '''
+
+
+def findMedianSortedArrays(nums1, nums2):
+    if len(nums1) > len(nums2):
+        return findMedianSortedArrays(nums2, nums1)
+
+    x, y = len(nums1), len(nums2)
+
+    lo, hi = 0, x
+
+    while lo <= hi:
+        partX = (lo + hi) // 2  # partition for shorter set
+        # partition for longer set including partX
+        partY = ((x + y + 1)) // 2 - partX
+
+        # create boundaries
+        maxLeftX = float("-inf") if partX == 0 else nums1[partX - 1]
+        minRightX = float("inf") if partX == x else nums1[partX]
+
+        maxLeftY = float("-inf") if partY == 0 else nums2[partY - 1]
+        minRightY = float("inf") if partY == y else nums2[partY]
+
+        if maxLeftX <= minRightY and maxLeftY <= minRightX:
+            if (x + y) % 2 == 0:
+                # even length
+                return float(max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2
+            else:
+                # odd length
+                return float(max(maxLeftX, maxLeftY))
+        elif maxLeftX > minRightY:
+            hi = partX - 1
+        else:
+            lo = partX + 1
