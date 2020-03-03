2635524b8fa37a5c8743f31878e31f9b91e15745
Ricky Lau <rickylaufitness@gmail.com>
2020-03-03 10:19:03 -0500

    edit: refactor minSubSumArrayLen js solution into arrow function

--- slidingWindow/minSubSumArrayLen.js
+++ slidingWindow/minSubSumArrayLen.js
@@ -1,5 +1,5 @@
 // write a function minimum sub array length that takes in an array and a number.
-// function should return the minimum array length that is greater than or equal to number.
+// function should return the minimum array length sum that is greater than or equal to number.
 // If there isnt it should return 0 otherwise
 
 // Ex 1) ([2,3,1], 1) => 1
@@ -12,29 +12,27 @@
 // SPACE COMPLEXITY
 // O(1) space
 
-function minSubArrayLen(arr, num) {
+const minSubSumArrayLen = (arr, num) => {
   let total = 0;
   let start = 0;
   let end = 0;
   let minLen = Infinity;
 
   while (start < arr.length) {
-    // if current window does not add up to sum, move window to right
+    // if current window is less than num - expand
     if (total < num && end < arr.length) {
       total += arr[end];
       end++;
     }
-    // if current window adds up to at least the sum given then shrink window
+    // if total is higher - shrink window
     else if (total >= num) {
       minLen = Math.min(minLen, end - start);
       total -= arr[start];
       start++;
     }
-    // current total is less than required and we have reached end
-    else {
-      break;
-    }
+    // current total is less than required and we reached end
+    else break;
   }
 
   return minLen === Infinity ? 0 : minLen;
-}
+};
