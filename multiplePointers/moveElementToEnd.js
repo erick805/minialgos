501c5b5dfe70a08299e50ea1ed924445d522e15e
Ricky Lau <rickylaufitness@gmail.com>
2020-03-04 14:07:50 -0500

    add: moveElementToEnd js linear multiple pointers solution

--- multiplePointers/moveElementToEnd.js
+++ multiplePointers/moveElementToEnd.js
@@ -6,3 +6,19 @@ the other integers.
 Input: array = [2,1,2,2,2,3,4,2], toMove = 2
 Output: [1,3,4,2,2,2,2,2] (the numbers 1,3 and 4 could be ordered differently)
 */
+
+const moveElementToEnd = (arr, toMove) => {
+  let beg = 0;
+  let end = arr.length - 1;
+
+  while (beg < end) {
+    while (beg < end && arr[end] === toMove) end--;
+    if (arr[beg] === toMove) {
+      const current = arr[beg];
+      arr[beg] = arr[end];
+      arr[end] = current;
+    }
+    beg++;
+  }
+  return arr;
+};
