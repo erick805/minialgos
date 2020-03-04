debabd2829f6ff73311a509ce85f6fefedd55aa8
Ricky Lau <rickylaufitness@gmail.com>
2020-03-04 12:26:22 -0500

    edit: time and space complexity to maxSubsetSumNoAdj py solution

--- multiplePointers/longestPalindrome.py
+++ multiplePointers/longestPalindrome.py
@@ -2,7 +2,7 @@
 Write a function that, given a string, returns its longest palindromic substring. A palindrome is defined as a string
 that is written the same forward and backward. Assume that there will only be one longest palindromic substring.
 
-Input: "abaxyzzyxf
+Input: "abaxyzzyxf"
 Output: "xyzzyx"
 '''
 
