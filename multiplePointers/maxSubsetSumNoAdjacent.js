32cc95e37bb12c7ccf81c710ded86097649fab5b
Ricky Lau <rickylaufitness@gmail.com>
2020-03-04 13:34:01 -0500

    add: maxSubsetSumNoAdjacent js linear and constant space solution

--- multiplePointers/maxSubsetSumNoAdjacent.js
+++ multiplePointers/maxSubsetSumNoAdjacent.js
@@ -4,3 +4,18 @@ Write a function that takes in an array of positive integers and returns an inte
 Input: [75,105,120,75,90,135]
 Output: 330 (75 + 120 + 135)
 */
+
+const maxSubsetSumNoAdjacent = array => {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+
+  let first = Math.max(array[0], array[1]);
+  let second = array[0];
+
+  for (let i = 2; i < array.length; i++) {
+    let current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+  return first;
+};
