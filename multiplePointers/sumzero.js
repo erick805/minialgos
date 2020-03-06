3291ff0976417ebc59ae70dba8141923d1273ffb
Ricky Lau <rickylaufitness@gmail.com>
2020-03-06 10:25:33 -0500

    edit: refactor into arrow function, and move sum to local while scope

--- multiplePointers/sumzero.js
+++ multiplePointers/sumzero.js
@@ -7,19 +7,15 @@
 // SPACE COMPLEXITY
 // O(1) space complexity
 
-function sumZero(arr) {
+const sumZero = arr => {
   let pt1 = 0;
   let pt2 = arr.length - 1;
-  let sum = arr[pt1] + arr[pt2];
 
-  while (pt2 > pt1) {
-    sum = arr[pt1] + arr[pt2];
-
+  while (pt1 < pt2) {
+    const sum = arr[pt1] + arr[pt2];
     if (sum === 0) return [arr[pt1], arr[pt2]];
-    else if (sum > 0) {
-      pt2--;
-    } else pt1++;
+    else if (sum > 0) pt2--;
+    else pt1++;
   }
-
-  // return undefined;
-}
+  return undefined;
+};
