cadb0e4c5611079001ee0a57dd357cf4e3b8ed4a
Ricky Lau <rickylaufitness@gmail.com>
2020-03-07 08:40:30 -0500

    create-add: branchSums js prompt and example into trees dir

--- trees/branchSums.js
+++ trees/branchSums.js
@@ -1,4 +1,4 @@
-'''
+/*
 Write a function that takes in a Binary Tree and returns a list of its branch sums(ordered from leftmost branch sum to rightmost branch sum).
 A branch sum is the sum of all values in a Binary Tree branch.
 A Binary Tree branch is a path of nodes in a tree that starts at the root node and ends at any leaf node.
@@ -14,31 +14,4 @@ Each Binary Tree node has a value stored in a property called "value" and two ch
     8   9   10
 
   Output: [15,16,18,10,11]
-'''
-
-# O(n) time | O(n) space
-class BinaryTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
-def branchSums(root):
-    sums = []
-    calculateBranchSums(root, 0, sums)
-    return sums
-
-
-def calculateBranchSums(node, runSum, sums):
-    if node is None:
-        return
-
-    newRunningSum = runSum + node.value
-
-    if node.left is None and node.right is None:
-        sums.append(newRunningSum)
-        return
-
-    calculateBranchSums(node.left, newRunningSum, sums)
-    calculateBranchSums(node.right, newRunningSum, sums)
+*/
