9e11968f99fd993a9ca589a585a5d4239a3286a7
Ricky Lau <rickylaufitness@gmail.com>
2020-03-08 12:54:19 -0400

    create-add: youngestCommonAncestor js prompt and example into trees dir

---
+++ trees/youngestCommonAncestor.js
@@ -0,0 +1,16 @@
+/*
+You're given three inputs, all of which are instances of a class that have an "ancestor" property pointing to their youngest ancestor.
+The first input is the top ancestor in an ancestral tree (i.e., the only instance that has no ancestor),
+and the other two inputs are descendants in the ancestral tree. Write a function that returns the youngest common ancestor to the two descendants.
+
+Input: Node A, Node E, Node I
+        A
+      /  \
+     B    C
+    / \  / \
+   D   EF   G
+  / \
+ H   I
+
+Output: Node B
+*/
