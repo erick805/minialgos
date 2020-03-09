0a6a0979ac356fdc55a2d9cfaf975c7de89c6d77
Ricky Lau <rickylaufitness@gmail.com>
2020-03-09 11:53:56 -0400

    edit-update: time and space complexity, refactor into arrow function, let to const num variable

--- findRecurring.js
+++ findRecurring.js
@@ -4,23 +4,18 @@
 //                 ([2,1,1,2,3,5,1,2,4]) => 1
 //                 ([2,3,4,5]) => undefined
 
-// TIME COMPLEXITY
-// O(n)
-
-// SPACE COMPLEXITY
-// O(n)
-
-function findRecurring(arr) {
+// O(arr.length) time | O(arr.length) space
+const findRecurring = arr => {
   // loop through the array and save each value as a key with a boolean true if seen.
   // if the dictionary contains a key with a boolean true on the next iteration of array.
   // we can return the key in dictionary or value of the array.
   // if no value is seen, return undefined.
   const seen = {};
   for (let i = 0; i < arr.length; i++) {
-    let num = arr[i];
+    const num = arr[i];
     if (!seen[num]) seen[num] = true;
     else if (seen[num]) return num;
   }
 
   return undefined;
-}
+};
