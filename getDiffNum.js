2c0763df16c785b00139acd335d300ed3fe8d5eb
Ricky Lau <rickylaufitness@gmail.com>
2020-03-09 12:07:42 -0400

    edit-add: prompt examples input and output, refactor getDiffNum to arrow, works with all test cases

--- getDiffNum.js
+++ getDiffNum.js
@@ -1,24 +1,20 @@
 /*
-Given an array arr of unique nonnegative integers, implement a function getDifferentNumber that finds the smallest nonnegative integer that is NOT in the array.
+Given an array of unique non-negative integers, implement a function getDiffNum that finds the smallest non-negative integer that is NOT in the array.
 
-conditional edge case if first element >= 1 return 0
-[1,2,3] => 0
-[10,50,51] => 0
+input: arr = [1, 2, 3, 4]
+output: 5
 
-
-let diff = arr[]
-input:  arr = [1, 2, 3, 4]
-
-output: 0
-
-input:  arr = [1, 2, 4]
-
-output: 0
-
-input:  arr = [0, 1, 2, 700]
-
+input: arr = [1, 2, 4]
 output: 3
 
+input: arr = [0, 1, 2, 700]
+output: 3
+
+input: arr = [700, 1, 3, 2]
+output: 4
+
+input: arr = [700, 699, 83, 85]
+output: 84
 */
 
 // Time Complexity
@@ -27,20 +23,16 @@ output: 3
 //Space Complexity
 //O(1) space
 
-function getDifferentNumber(arr) {
-  //sort the array first
-  arr = arr.sort();
-  if (arr[0] >= 1) return 0;
+const getDiffNum = arr => {
+  arr.sort((a, b) => a - b);
+  let curVal = arr[0];
 
-  let currVal = arr[0];
+  if (arr.length === 1) return currVal + 1;
 
-  if (arr.length === 1) return arr[0] + 1;
-
-  // Check if each element is inrementing by 1, if so stop as soon as element stops incrementing
-  // return element + 1
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] + 1 === arr[i + 1]) currVal = arr[i + 1];
+    if (arr[i] + 1 === arr[i + 1]) curVal = arr[i + 1];
     else break;
   }
-  return currVal + 1;
-}
+
+  return curVal + 1;
+};
